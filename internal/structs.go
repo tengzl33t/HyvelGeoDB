77de8462c8a026935ce75c936858f6ccefa2afb8
Tengz <redcraftrus@gmail.com>
2025-05-27 13:08:37 +0300

    cleanup

--- internal/structs.go
+++ internal/structs.go
@@ -16,37 +16,6 @@ type ASNDBStruct struct {
 	IPRanges map[string]IPRangeASNStruct
 }
 
-type IPRangeCountryStruct struct {
-	Range       netip.Prefix
-	CountryCode string
-}
-
-type LocationStruct struct {
-	Code  string
-	Names map[string]string
-}
-
-type CountryDBStruct struct {
-	Name     string
-	IPRanges map[string]IPRangeCountryStruct
-}
-
-type InDBStruct struct {
-	Priority int
-	Path     string
-}
-
-type MMDBCountryStruct struct {
-	GeoNameID int               `json:"geoname_id"`
-	ISOCode   string            `json:"iso_code"`
-	Names     map[string]string `json:"names"`
-}
-
-type MMDBASNStruct struct {
-	ASN int    `json:"autonomous_system_number"`
-	Org string `json:"autonomous_system_organization"`
-}
-
 type DBStruct struct {
 	Name               string
 	Path               string
@@ -55,9 +24,4 @@ type DBStruct struct {
 	Reader             interface{}
 	CountrySearchPaths []string
 	ASNSearchPaths     []string
-}
-
-type LocationResultStruct struct {
-	Location string
-	Priority int
 }
